eae6d90f2b385fba5831229068bec9009d925799
qixiaoling <qixiaolinglinda@hotmail.com>
2022-02-21 10:52:28 +0100

    First commit, build but cannot run yet

---
+++ src/main/java/qi.xiaoling/Config/DispatcherServletInit.java
@@ -0,0 +1,21 @@
+package qi.xiaoling.Config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebApplicationContextConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/qi.xiaoling/Config/WebApplicationContextConfig.java
@@ -0,0 +1,24 @@
+package qi.xiaoling.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("qi.xiaoling")
+public class WebApplicationContextConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+
+}
